fe16870c55fe2937d35314980542bbf2e1eda2a5
Ozan Erdal <oerdal@Ozans-MacBook-Pro.local>
2018-11-13 20:41:54 -0800

    Updates

---
+++ precache-manifest.f7e5512092d52606eef49a3cab06e79b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d2e3892f127a791bf2405ba700c6fc6",
+    "url": "/my-app/static/media/tree.6d2e3892.jpg"
+  },
+  {
+    "revision": "6c94ef1c229dd69fe30dc0bed3f25f16",
+    "url": "/my-app/static/media/ozan.6c94ef1c.jpeg"
+  },
+  {
+    "revision": "711af32e0007a730adf06757154be75d",
+    "url": "/my-app/static/media/ocean.711af32e.jpg"
+  },
+  {
+    "revision": "cd62aa8f468958a5803989e90975a66d",
+    "url": "/my-app/static/media/iconmonstr-github.cd62aa8f.png"
+  },
+  {
+    "revision": "e6a00cd4c24e5948ae87fe3baba3548c",
+    "url": "/my-app/static/media/flowers.e6a00cd4.jpg"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "76ab4bf5c367ca466dd2",
+    "url": "/my-app/static/js/main.76ab4bf5.chunk.js"
+  },
+  {
+    "revision": "53beba31f6fd6e7f3404",
+    "url": "/my-app/static/js/1.53beba31.chunk.js"
+  },
+  {
+    "revision": "76ab4bf5c367ca466dd2",
+    "url": "/my-app/static/css/main.cfee00f4.chunk.css"
+  },
+  {
+    "revision": "5e471a8e8028dd1cf0b46d0dc80db161",
+    "url": "/my-app/index.html"
+  }
+];
